0043c51f12fa70751dc65cbb2928d824e9aa4264
Kingdon Barrett <kingdon.b@nd.edu>
2020-08-15 17:19:18 -0400

    200 OK: /healthz

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'regions#index'
   get '/regions', to: 'regions#index'
   get '/regions/:id', to: 'regions#show'
+
+  get '/healthz', to: proc { [200, {}, ['']] }
 end
