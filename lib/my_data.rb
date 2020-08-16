a21deec38631dc0c982b31c78afbacec8ac42386
Kingdon Barrett <kingdon.b@nd.edu>
2020-08-16 11:51:35 -0400

    TableLoader consumes rows, saves SampleData models

--- lib/my_data.rb
+++ lib/my_data.rb
@@ -1,9 +1,11 @@
 require 'csv'
 
 class MyData
+  attr_reader :rows
+
   def initialize
     @data = []
-    @file =
+    @rows =
       CSV.read(Rails.root.join('lib', 'data.csv'), headers: true)
   end
 end
